4ffa2200fa3e1881cbb014c7044be86e6cedd671
Niko Raes <niko.raes@arcadis.com>
2024-12-17 09:43:22 +0100

    formatting

--- JexlNet.Test/ExtendedGrammar.cs
+++ JexlNet.Test/ExtendedGrammar.cs
@@ -137,7 +137,10 @@ public class ExtendedGrammarUnitTest
     [InlineData("['foo','bar']|join('-')", "foo-bar")]
     [InlineData("'f,b,a,d,e,c'|split(',')|sort|join", "a,b,c,d,e,f")]
     [InlineData("'f,b,a,d,e,c'|split(',')|sort|join('')", "abcdef")]
-    [InlineData("'2024-07-08 23:50:00'|split(' ')|join('T') + '.00000+02:00'", "2024-07-08T23:50:00.00000+02:00")]
+    [InlineData(
+        "'2024-07-08 23:50:00'|split(' ')|join('T') + '.00000+02:00'",
+        "2024-07-08T23:50:00.00000+02:00"
+    )]
     public void Join(string expression, string expected)
     {
         var jexl = new Jexl(new ExtendedGrammar());
@@ -177,9 +180,14 @@ public class ExtendedGrammarUnitTest
     [Theory]
     [InlineData("'foobar'|regexMatches('foo')|mapField('0')|includes('foo')", "true")]
     [InlineData("'foobar'|regexMatches('baz')|mapField('0')|includes('foo')", "false")]
-    [InlineData("'<table><tr><td>foo1</td><td>bar1</td></tr><tr><td>foo2</td><td>bar2</td></tr><tr><td>foo3</td><td>bar3</td></tr></table>'|regexMatches('<tr><td>(?<col1>.*?)</td><td>(?<col2>.*?)</td></tr>')", @"[{""0"":""<tr><td>foo1</td><td>bar1</td></tr>"",""col1"":""foo1"",""col2"":""bar1""},{""0"":""<tr><td>foo2</td><td>bar2</td></tr>"",""col1"":""foo2"",""col2"":""bar2""},{""0"":""<tr><td>foo3</td><td>bar3</td></tr>"",""col1"":""foo3"",""col2"":""bar3""}]")]
-    [InlineData("'<table><tr><td>foo1</td><td>bar1</td></tr><tr><td>foo2</td><td>bar2</td></tr><tr><td>foo3</td><td>bar3</td></tr></table>'|regexMatches('<tr><td>(?<col1>.*?)</td>(?:<td>(?<col2>.*?)</td>)?</tr>')", @"[{""0"":""<tr><td>foo1</td><td>bar1</td></tr>"",""col1"":""foo1"",""col2"":""bar1""},{""0"":""<tr><td>foo2</td><td>bar2</td></tr>"",""col1"":""foo2"",""col2"":""bar2""},{""0"":""<tr><td>foo3</td><td>bar3</td></tr>"",""col1"":""foo3"",""col2"":""bar3""}]")]
-
+    [InlineData(
+        "'<table><tr><td>foo1</td><td>bar1</td></tr><tr><td>foo2</td><td>bar2</td></tr><tr><td>foo3</td><td>bar3</td></tr></table>'|regexMatches('<tr><td>(?<col1>.*?)</td><td>(?<col2>.*?)</td></tr>')",
+        @"[{""0"":""<tr><td>foo1</td><td>bar1</td></tr>"",""col1"":""foo1"",""col2"":""bar1""},{""0"":""<tr><td>foo2</td><td>bar2</td></tr>"",""col1"":""foo2"",""col2"":""bar2""},{""0"":""<tr><td>foo3</td><td>bar3</td></tr>"",""col1"":""foo3"",""col2"":""bar3""}]"
+    )]
+    [InlineData(
+        "'<table><tr><td>foo1</td><td>bar1</td></tr><tr><td>foo2</td><td>bar2</td></tr><tr><td>foo3</td><td>bar3</td></tr></table>'|regexMatches('<tr><td>(?<col1>.*?)</td>(?:<td>(?<col2>.*?)</td>)?</tr>')",
+        @"[{""0"":""<tr><td>foo1</td><td>bar1</td></tr>"",""col1"":""foo1"",""col2"":""bar1""},{""0"":""<tr><td>foo2</td><td>bar2</td></tr>"",""col1"":""foo2"",""col2"":""bar2""},{""0"":""<tr><td>foo3</td><td>bar3</td></tr>"",""col1"":""foo3"",""col2"":""bar3""}]"
+    )]
     public void RegexMatches(string expression, string expected)
     {
         var jexl = new Jexl(new ExtendedGrammar());
@@ -246,7 +254,6 @@ public class ExtendedGrammarUnitTest
         Assert.Equal(expected, result?.AsValue().ToDecimal());
     }
 
-
     [Theory]
     [InlineData("1|toBoolean", true)]
     [InlineData("3|toBoolean", true)]
@@ -292,9 +299,13 @@ public class ExtendedGrammarUnitTest
     [InlineData("{foo:0, bar:1, baz:2, tek:3}|keys")]
     [InlineData("{a:'foo', b:'bar', c:'baz', d:'tek'}|values")]
     [InlineData("[{name:'foo'}, {name:'bar'}, {name:'baz'}, {name:'tek'}]|mapField('name')")]
-    [InlineData("[{name:'tek',age:32}, {name:'bar',age:34}, {name:'baz',age:33}, {name:'foo',age:35}]|sort('age',true)|mapField('name')")]
+    [InlineData(
+        "[{name:'tek',age:32}, {name:'bar',age:34}, {name:'baz',age:33}, {name:'foo',age:35}]|sort('age',true)|mapField('name')"
+    )]
     [InlineData("['foo']|append(['tek','baz','bar']|sort)")]
-    [InlineData("['foo']|append(['tek', 'baz', 'bar', 'foo', 'foo']|filter('value != \\'foo\\'')|sort)")]
+    [InlineData(
+        "['foo']|append(['tek', 'baz', 'bar', 'foo', 'foo']|filter('value != \\'foo\\'')|sort)"
+    )]
     public void Arrays(string expression)
     {
         var jexl = new Jexl(new ExtendedGrammar());
@@ -304,7 +315,10 @@ public class ExtendedGrammarUnitTest
     }
 
     [Theory]
-    [InlineData("[{name:'foo'}, {name:'bar'}, {name:'baz'}, {name:'tek'}]|map('value.name')", "['foo', 'bar', 'baz', 'tek']")]
+    [InlineData(
+        "[{name:'foo'}, {name:'bar'}, {name:'baz'}, {name:'tek'}]|map('value.name')",
+        "['foo', 'bar', 'baz', 'tek']"
+    )]
     [InlineData("assoc|map('value.age')", "[32,34,45]")]
     [InlineData("assoc|map('value.lastName')", "['Archer','Poovey','Figgis']")]
     [InlineData("assoc|map('value.age + index')", "[32,35,47]")]
@@ -312,11 +326,15 @@ public class ExtendedGrammarUnitTest
     [InlineData("assoc|map('value.age')|avg", "37")]
     public void Map(string expression, string expected)
     {
-        var context = new JsonObject {
-            { "assoc", new JsonArray {
-                new JsonObject {{ "lastName", "Archer" }, {"age", 32 }},
-                new JsonObject {{ "lastName", "Poovey" }, {"age", 34 }},
-                new JsonObject {{ "lastName", "Figgis" }, {"age", 45 }},
+        var context = new JsonObject
+        {
+            {
+                "assoc",
+                new JsonArray
+                {
+                    new JsonObject { { "lastName", "Archer" }, { "age", 32 } },
+                    new JsonObject { { "lastName", "Poovey" }, { "age", 34 } },
+                    new JsonObject { { "lastName", "Figgis" }, { "age", 45 } },
                 }
             }
         };
@@ -327,7 +345,10 @@ public class ExtendedGrammarUnitTest
     }
 
     [Theory]
-    [InlineData("[{name:'foo'}, {name:'bar'}, {name:'baz'}, {name:'tek'}]|any('value.name==\\'foo\\'')", true)]
+    [InlineData(
+        "[{name:'foo'}, {name:'bar'}, {name:'baz'}, {name:'tek'}]|any('value.name==\\'foo\\'')",
+        true
+    )]
     [InlineData("assoc|every('value.age>30')", true)]
     [InlineData("assoc|every('value.age>40')", false)]
     [InlineData("assoc|some('value.age>40')", true)]
@@ -335,11 +356,15 @@ public class ExtendedGrammarUnitTest
     [InlineData("assoc|map('value.age')|some('value>30')", true)]
     public void AnyAll(string expression, bool expected)
     {
-        var context = new JsonObject {
-            { "assoc", new JsonArray {
-                new JsonObject {{ "lastName", "Archer" }, {"age", 32 }},
-                new JsonObject {{ "lastName", "Poovey" }, {"age", 34 }},
-                new JsonObject {{ "lastName", "Figgis" }, {"age", 45 }},
+        var context = new JsonObject
+        {
+            {
+                "assoc",
+                new JsonArray
+                {
+                    new JsonObject { { "lastName", "Archer" }, { "age", 32 } },
+                    new JsonObject { { "lastName", "Poovey" }, { "age", 34 } },
+                    new JsonObject { { "lastName", "Figgis" }, { "age", 45 } },
                 }
             }
         };
@@ -350,15 +375,25 @@ public class ExtendedGrammarUnitTest
 
     [Theory]
     [InlineData("assoc|reduce('accumulator + value.age', 0)", "111")]
-    [InlineData("assoc|reduce('value.age > array|map(\\'value.age\\')|avg ? accumulator|append(value.age) : accumulator', [])", "[45]")]
-    [InlineData("assoc|reduce('value.age < array|map(\\'value.age\\')|avg ? accumulator|append(value.age) : accumulator', [])[1]", "34")]
+    [InlineData(
+        "assoc|reduce('value.age > array|map(\\'value.age\\')|avg ? accumulator|append(value.age) : accumulator', [])",
+        "[45]"
+    )]
+    [InlineData(
+        "assoc|reduce('value.age < array|map(\\'value.age\\')|avg ? accumulator|append(value.age) : accumulator', [])[1]",
+        "34"
+    )]
     public void Reduce(string expression, string expected)
     {
-        var context = new JsonObject {
-            { "assoc", new JsonArray {
-                new JsonObject {{ "lastName", "Archer" }, {"age", 32 }},
-                new JsonObject {{ "lastName", "Poovey" }, {"age", 34 }},
-                new JsonObject {{ "lastName", "Figgis" }, {"age", 45 }},
+        var context = new JsonObject
+        {
+            {
+                "assoc",
+                new JsonArray
+                {
+                    new JsonObject { { "lastName", "Archer" }, { "age", 32 } },
+                    new JsonObject { { "lastName", "Poovey" }, { "age", 34 } },
+                    new JsonObject { { "lastName", "Figgis" }, { "age", 45 } },
                 }
             }
         };
@@ -395,10 +430,19 @@ public class ExtendedGrammarUnitTest
 
     [Theory]
     [InlineData("(now()|toMillis / 1000)|ceil == (millis() / 1000)|ceil", true)]
-    [InlineData("(((millis() / 1000) | ceil) * 1000) | toDateTime == ((now()|toMillis / 1000) | ceil * 1000) | toDateTime", true)]
-    [InlineData("(((millis() / 1000) | ceil) * 1000) | toDateTime | dateTimeAdd('second',5) == (((now()|toMillis / 1000) + 5) | ceil * 1000) | toDateTime", true)]
+    [InlineData(
+        "(((millis() / 1000) | ceil) * 1000) | toDateTime == ((now()|toMillis / 1000) | ceil * 1000) | toDateTime",
+        true
+    )]
+    [InlineData(
+        "(((millis() / 1000) | ceil) * 1000) | toDateTime | dateTimeAdd('second',5) == (((now()|toMillis / 1000) + 5) | ceil * 1000) | toDateTime",
+        true
+    )]
     [InlineData("'22-Feb-24 00:00:00'|toDateTime == '2024-02-22T00:00:00Z'|toDateTime", true)] // this is just a coincidence that this works, it won't in JS
-    [InlineData("'02-22-24 00:00:00'|toDateTime('MM-dd-yy HH:mm:ss') == '2024-02-22T00:00:00Z'|toDateTime", true)]
+    [InlineData(
+        "'02-22-24 00:00:00'|toDateTime('MM-dd-yy HH:mm:ss') == '2024-02-22T00:00:00Z'|toDateTime",
+        true
+    )]
     [InlineData("(now()|toMillis - 1000) > (millis() - 2000)", true)]
     [InlineData("$toDateTime(1727169975224)|toMillis == 1727169975224", true)]
     public void TimeFunctions(string expression, bool expected)
@@ -410,7 +454,10 @@ public class ExtendedGrammarUnitTest
 
     [Theory]
     [InlineData("'22-Feb-24 00:00:00'|toDateTime", "2024-02-22T00:00:00.0000000+00:00")]
-    [InlineData("'02-22-24 00:00:00'|toDateTime('MM-dd-yy HH:mm:ss')", "2024-02-22T00:00:00.0000000+00:00")]
+    [InlineData(
+        "'02-22-24 00:00:00'|toDateTime('MM-dd-yy HH:mm:ss')",
+        "2024-02-22T00:00:00.0000000+00:00"
+    )]
     public void TimeFunctions2(string expression, string expected)
     {
         var jexl = new Jexl(new ExtendedGrammar());
@@ -424,11 +471,15 @@ public class ExtendedGrammarUnitTest
     [InlineData("assoc[2]|eval(expression)", "45")]
     public void Eval(string expression, string expected)
     {
-        var context = new JsonObject {
-            { "assoc", new JsonArray {
-                new JsonObject {{ "lastName", "Archer" }, {"age", 32 }},
-                new JsonObject {{ "lastName", "Poovey" }, {"age", 34 }},
-                new JsonObject {{ "lastName", "Figgis" }, {"age", 45 }},
+        var context = new JsonObject
+        {
+            {
+                "assoc",
+                new JsonArray
+                {
+                    new JsonObject { { "lastName", "Archer" }, { "age", 32 } },
+                    new JsonObject { { "lastName", "Poovey" }, { "age", 34 } },
+                    new JsonObject { { "lastName", "Figgis" }, { "age", 45 } },
                 }
             },
             { "expression", "age" }
@@ -462,10 +513,20 @@ public class ExtendedGrammarUnitTest
     public void ComplexMapExample1()
     {
         var jexl = new Jexl(new ExtendedGrammar());
-        string context = """{"timestamp":{"low":1710760508,"high":0,"unsigned":true},"data":{"id":"176971594","tripUpdate":{"trip":{"tripId":"176971594","startTime":"15:18:00","startDate":"20240318","scheduleRelationship":"SCHEDULED","routeId":"90296","directionId":0},"stopTimeUpdate":[{"scheduleRelationship":"SCHEDULED","stopId":"2511612","departure":{"time":"1710771480","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2628927","departure":{"time":"1710771660","delay":0},"arrival":{"time":"1710771660","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2628499","departure":{"time":"1710772080","delay":0},"arrival":{"time":"1710772080","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2510883","departure":{"time":"1710772380","delay":0},"arrival":{"time":"1710772320","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2510417","departure":{"time":"1710772980","delay":0},"arrival":{"time":"1710772920","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2511520","arrival":{"time":"1710773400","delay":0}}]}},"gtfsRealtimeVersion":"1.0","incrementality":0}""";
-        string expression = """data.tripUpdate.stopTimeUpdate|map(data.tripUpdate.trip.tripId + '+ \'-\' + value.stopId')""";
+        string context =
+            """{"timestamp":{"low":1710760508,"high":0,"unsigned":true},"data":{"id":"176971594","tripUpdate":{"trip":{"tripId":"176971594","startTime":"15:18:00","startDate":"20240318","scheduleRelationship":"SCHEDULED","routeId":"90296","directionId":0},"stopTimeUpdate":[{"scheduleRelationship":"SCHEDULED","stopId":"2511612","departure":{"time":"1710771480","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2628927","departure":{"time":"1710771660","delay":0},"arrival":{"time":"1710771660","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2628499","departure":{"time":"1710772080","delay":0},"arrival":{"time":"1710772080","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2510883","departure":{"time":"1710772380","delay":0},"arrival":{"time":"1710772320","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2510417","departure":{"time":"1710772980","delay":0},"arrival":{"time":"1710772920","delay":0}},{"scheduleRelationship":"SCHEDULED","stopId":"2511520","arrival":{"time":"1710773400","delay":0}}]}},"gtfsRealtimeVersion":"1.0","incrementality":0}""";
+        string expression =
+            """data.tripUpdate.stopTimeUpdate|map(data.tripUpdate.trip.tripId + '+ \'-\' + value.stopId')""";
         var result = jexl.Eval(expression, context);
-        var expected = new JsonArray { "176971594-2511612", "176971594-2628927", "176971594-2628499", "176971594-2510883", "176971594-2510417", "176971594-2511520" };
+        var expected = new JsonArray
+        {
+            "176971594-2511612",
+            "176971594-2628927",
+            "176971594-2628499",
+            "176971594-2510883",
+            "176971594-2510417",
+            "176971594-2511520"
+        };
         Assert.True(JsonNode.DeepEquals(expected, result));
     }
 
@@ -473,8 +534,10 @@ public class ExtendedGrammarUnitTest
     public void ComplexMapExample2()
     {
         var jexl = new Jexl(new ExtendedGrammar());
-        string context = """{"data": [{"id": 18833,"name": "NL-238409_RAW_3296_08","variableId": 167147,"variableName": "3296_08_X", "data": {   "2024-09-05 13:25:00": 1992.5402 } }, {  "id": 18833, "name": "NL-238409_RAW_3296_08", "variableId": 167148, "variableName": "3296_08_Y", "data": {         "2024-09-05 13:25:00": 5090.9231  }  }]}""";
-        string expression = """data|find('!value.variableName|startsWith(\'d\') && value.variableName|endsWith(\'X\')').data|values[0]""";
+        string context =
+            """{"data": [{"id": 18833,"name": "NL-238409_RAW_3296_08","variableId": 167147,"variableName": "3296_08_X", "data": {   "2024-09-05 13:25:00": 1992.5402 } }, {  "id": 18833, "name": "NL-238409_RAW_3296_08", "variableId": 167148, "variableName": "3296_08_Y", "data": {         "2024-09-05 13:25:00": 5090.9231  }  }]}""";
+        string expression =
+            """data|find('!value.variableName|startsWith(\'d\') && value.variableName|endsWith(\'X\')').data|values[0]""";
         var result = jexl.Eval(expression, context);
         var expected = 1992.5402;
         Assert.Equal(expected, result.AsValue().ToDouble());
@@ -484,21 +547,23 @@ public class ExtendedGrammarUnitTest
     public void ComplexExample3()
     {
         var jexl = new Jexl(new ExtendedGrammar());
-        string context = """{"data": "204016                        52172    004   002ID  87362729       J         07162024MX                                        ID  132634661      J54G2   N   90                        07162024                                             ORINWN                                                                                                             "}""";
+        string context =
+            """{"data": "204016                        52172    004   002ID  87362729       J         07162024MX                                        ID  132634661      J54G2   N   90                        07162024                                             ORINWN                                                                                                             "}""";
         Assert.Equal("004", jexl.Eval("data|substring(39,6)|trim", context).ToString());
 
-        string expression = """data|substring(0,30)|trim + '-' + data|substring(39,6)|trim + '-' + data|substring(45,3)|trim""";
+        string expression =
+            """data|substring(0,30)|trim + '-' + data|substring(39,6)|trim + '-' + data|substring(45,3)|trim""";
         var result = jexl.Eval(expression, context);
         var expected = "204016-004-002";
         Assert.Equal(expected, result.ToString());
     }
 
-
     [Fact]
     public void ComplexExample4()
     {
         var jexl = new Jexl(new ExtendedGrammar());
-        string context = @"{  ""fields"": {
+        string context =
+            @"{  ""fields"": {
     ""_ch1"": ""18.8929"",
     ""_ch2"": ""16.142"",
     ""ch1"": {
@@ -511,8 +576,9 @@ public class ExtendedGrammarUnitTest
     }
     }
 }";
-        //"!value[0]|startsWith(\"_\") && " + 
-        string expression = """fields|entries|map('[array|find("value[0]==\""+value[0]|substring(1)+"\"")[1].field,value[1]]')|filter('!!value[0]')|toObject""";
+        //"!value[0]|startsWith(\"_\") && " +
+        string expression =
+            """fields|entries|map('[array|find("value[0]==\""+value[0]|substring(1)+"\"")[1].field,value[1]]')|filter('!!value[0]')|toObject""";
         var result = jexl.Eval(expression, context);
         var expected = new JsonObject { { "LEVEL", "18.8929" }, { "TEMPERATURE", "16.142" } };
         Assert.True(JsonNode.DeepEquals(expected, result));
@@ -522,7 +588,8 @@ public class ExtendedGrammarUnitTest
     public void ComplexExample5()
     {
         var jexl = new Jexl(new ExtendedGrammar());
-        string context = @"{    ""evalInput"": {
+        string context =
+            @"{    ""evalInput"": {
     ""data"": {
       ""measurementTimeDefault"": ""2024-12-05T14:05:00Z"",
       ""measuredValue"": [
@@ -547,15 +614,18 @@ public class ExtendedGrammarUnitTest
     ]
     } } }";
 
-        string expression = """(5 + {a:5}|eval(evalInput.data.measuredValue|filter('value["@index"]=="6"')[0].measuredValue.basicData.vehicleFlow.vehicleFlowRate + ' - a'))|toString""";
+        string expression =
+            """(5 + {a:5}|eval(evalInput.data.measuredValue|filter('value["@index"]=="6"')[0].measuredValue.basicData.vehicleFlow.vehicleFlowRate + ' - a'))|toString""";
         var result = jexl.Eval(expression, context);
         Assert.Equal("180", result.ToString());
     }
 
-
     [Theory]
     [InlineData("['f','o','o']|map((v,i) => v + i)|join('')", "f0o1o2")]
-    [InlineData("['foo','bar','baz','tek']|reduce((acc,v) => acc + '-' + v,'abc')", "abc-foo-bar-baz-tek")]
+    [InlineData(
+        "['foo','bar','baz','tek']|reduce((acc,v) => acc + '-' + v,'abc')",
+        "abc-foo-bar-baz-tek"
+    )]
     public void ArrowOperatorString(string expression, string expected)
     {
         var jexl = new Jexl(new ExtendedGrammar());
